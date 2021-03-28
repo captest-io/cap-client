b8c096092d1f8a08507913bd6249114095a84e76
tkonopka <tokonopka@gmail.com>
2021-03-28 18:05:30 +0100

    more validations of command-line inputs

--- cap_client/validations.py
+++ cap_client/validations.py
@@ -10,12 +10,19 @@ def validate_config(config):
     if config.action in ("publish", "create"):
         if config.collection is None:
             raise ValidationError("missing --collection")
+    if config.action == "datafiles":
+        if config.parent_uuid is None:
+            raise ValidationError("missing --parent_uuid")
     if config.file is not None:
         if not isfile(config.file):
             raise ValidationError("file does not exist: "+str(config.file))
     if config.dir is not None:
         if not isdir(config.dir):
             raise ValidationError("directory does not exist: "+str(config.dir))
+    if not config.api.endswith("/"):
+        config.api += "/"
+    if not config.api.startswith("http"):
+        config.api = "http://"+config.api
     return config
 
 
---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+setup(name="cap-client",
+      license="BSD",
+      packages=["cap_client"])
